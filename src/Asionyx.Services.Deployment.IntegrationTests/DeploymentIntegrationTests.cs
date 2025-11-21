f6540ccfc9f8bae6f6355caa44bd98fcc8be9e7f
50chickens <50chickens@gmail.com>
2025-11-21 16:19:58 +1100

    fixes

--- src/Asionyx.Services.Deployment.IntegrationTests/DeploymentIntegrationTests.cs
+++ src/Asionyx.Services.Deployment.IntegrationTests/DeploymentIntegrationTests.cs
@@ -12,16 +12,22 @@ namespace Asionyx.Services.Deployment.IntegrationTests;
 [Category("Integration")]
 public class DeploymentIntegrationTests
 {
+    private System.Net.Http.HttpClient Client;
+
     [OneTimeSetUp]
     public async Task OneTimeSetUp() => await IntegrationTestSetup.EnsureInfoAvailableAsync();
+
+    [SetUp]
+    public void SetUp()
+    {
+        Client = IntegrationTestSetup.Client;
+        Assert.That(Client, Is.Not.Null, "HttpClient must be initialized by IntegrationTestSetup");
+    }
 
     [Test]
     public async Task Info_Returns_Service_Info()
     {
-        var client = IntegrationTestSetup.Client;
-        Assert.That(client, Is.Not.Null, "HttpClient must be initialized by IntegrationTestSetup");
-
-        var resp = await client.GetAsync("/info");
+        var resp = await Client.GetAsync("/info");
         resp.EnsureSuccessStatusCode();
         var body = await resp.Content.ReadAsStringAsync();
         Assert.That(body, Does.Contain("Asionyx.Services.Deployment"));
@@ -30,13 +36,10 @@ public class DeploymentIntegrationTests
     [Test]
     public async Task Status_Returns_Status()
     {
-        var client = IntegrationTestSetup.Client;
-        Assert.That(client, Is.Not.Null, "HttpClient must be initialized by IntegrationTestSetup");
-
-        var statusResp = await client.GetAsync("/status");
+        var statusResp = await Client.GetAsync("/status");
         if (!statusResp.IsSuccessStatusCode && statusResp.StatusCode == System.Net.HttpStatusCode.Unauthorized)
         {
-            statusResp = await TestHelpers.RunWithoutApiKeyAsync(client, () => client.GetAsync("/status"));
+            statusResp = await TestHelpers.RunWithoutApiKeyAsync(Client, () => Client.GetAsync("/status"));
         }
         statusResp.EnsureSuccessStatusCode();
         var statusBody = await statusResp.Content.ReadAsStringAsync();
@@ -46,12 +49,9 @@ public class DeploymentIntegrationTests
     [Test]
     public async Task Systemd_Start_HelloWorld()
     {
-        var client = IntegrationTestSetup.Client;
-        Assert.That(client, Is.Not.Null, "HttpClient must be initialized by IntegrationTestSetup");
-
         var payload = new { Action = "start", Name = "Asionyx.Services.HelloWorld" };
         var content = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json");
-        var sysResp = await client.PostAsync("/systemd", content);
+        var sysResp = await Client.PostAsync("/systemd", content);
         sysResp.EnsureSuccessStatusCode();
         var sysBody = await sysResp.Content.ReadAsStringAsync();
         Assert.That(sysBody, Does.Contain("started") | Does.Contain("already running") | Does.Contain("Executable not found"));
@@ -60,24 +60,21 @@ public class DeploymentIntegrationTests
     [Test]
     public async Task Packages_Install_List_Remove()
     {
-        var client = IntegrationTestSetup.Client;
-        Assert.That(client, Is.Not.Null, "HttpClient must be initialized by IntegrationTestSetup");
-
         var installPayload = new { Action = "install", Packages = new[] { "sl" } };
         var pkgInstallReq = new StringContent(JsonConvert.SerializeObject(installPayload), Encoding.UTF8, "application/json");
-        var pkgInstallResp = await client.PostAsync("/packages", pkgInstallReq);
+        var pkgInstallResp = await Client.PostAsync("/packages", pkgInstallReq);
         pkgInstallResp.EnsureSuccessStatusCode();
         var pkgInstallBody = await pkgInstallResp.Content.ReadAsStringAsync();
         Assert.That(pkgInstallBody, Does.Contain("installed") | Does.Contain("Setting up") | Does.Contain("is already the newest"));
 
-        var pkgListResp = await client.GetAsync("/packages");
+        var pkgListResp = await Client.GetAsync("/packages");
         pkgListResp.EnsureSuccessStatusCode();
         var pkgListBody = await pkgListResp.Content.ReadAsStringAsync();
         Assert.That(pkgListBody, Does.Contain("sl") | Does.Contain(":/usr/games/sl") | Does.Contain("sl -"));
 
         var removePayload = new { Action = "remove", Packages = new[] { "sl" } };
         var pkgRemoveReq = new StringContent(JsonConvert.SerializeObject(removePayload), Encoding.UTF8, "application/json");
-        var pkgRemoveResp = await client.PostAsync("/packages", pkgRemoveReq);
+        var pkgRemoveResp = await Client.PostAsync("/packages", pkgRemoveReq);
         pkgRemoveResp.EnsureSuccessStatusCode();
         var pkgRemoveBody = await pkgRemoveResp.Content.ReadAsStringAsync();
         Assert.That(pkgRemoveBody, Does.Contain("removed") | Does.Contain("Removing") | Does.Contain("not installed"));
@@ -86,37 +83,31 @@ public class DeploymentIntegrationTests
     [Test]
     public async Task Filesystem_Write_Read_Delete()
     {
-        var client = IntegrationTestSetup.Client;
-        Assert.That(client, Is.Not.Null, "HttpClient must be initialized by IntegrationTestSetup");
-
         var testPath = "/tmp/asionyx_integration_test.txt";
         var writePayload = new { Action = "write", Path = testPath, Content = "hello-asio" };
         var writeReq = new StringContent(JsonConvert.SerializeObject(writePayload), Encoding.UTF8, "application/json");
-        var writeResp = await client.PostAsync("/filesystem/files", writeReq);
+        var writeResp = await Client.PostAsync("/filesystem/files", writeReq);
         writeResp.EnsureSuccessStatusCode();
 
-        var readResp = await client.GetAsync($"/filesystem/files?path={Uri.EscapeDataString(testPath)}");
+        var readResp = await Client.GetAsync($"/filesystem/files?path={Uri.EscapeDataString(testPath)}");
         readResp.EnsureSuccessStatusCode();
         var readBody = await readResp.Content.ReadAsStringAsync();
         Assert.That(readBody, Does.Contain("hello-asio"));
 
         var delReq = new StringContent(JsonConvert.SerializeObject(new { Action = "delete", Path = testPath }), Encoding.UTF8, "application/json");
-        var delResp = await client.PostAsync("/filesystem/files", delReq);
+        var delResp = await Client.PostAsync("/filesystem/files", delReq);
         delResp.EnsureSuccessStatusCode();
     }
 
     [Test]
     public async Task Package_Upload()
     {
-        var client = IntegrationTestSetup.Client;
-        Assert.That(client, Is.Not.Null, "HttpClient must be initialized by IntegrationTestSetup");
-
         var nupkgPath = TestHelpers.CreateTestNupkg();
         var (form, fs) = TestHelpers.CreatePackageForm(nupkgPath);
         using (form)
         using (fs)
         {
-            var uploadResp = await client.PostAsync("/package", form);
+            var uploadResp = await Client.PostAsync("/package", form);
             uploadResp.EnsureSuccessStatusCode();
             var uploadBody = await uploadResp.Content.ReadAsStringAsync();
             Assert.That(uploadBody, Does.Contain("testpkg") | Does.Contain("manifest"));
